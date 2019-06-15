a1cc2996d21529798fe80d9c68853e878eb9c7aa
krzysztofson <krzysztofson@gmail.com>
2019-06-15 16:44:56 +0200

    display btn value when clicked

--- app/js/calculator.js
+++ app/js/calculator.js
@@ -4,6 +4,34 @@ const functionBtns = document.querySelectorAll('[data-function]');
 const acBtn = document.querySelector('[data-allclear]');
 const saveBtn = document.querySelector('[data-save]');
 const resultBtn = document.querySelector('[data-result]');
-const beforeScreen = document.querySelector('[data-before]');
-const nowScreen = document.querySelector('[data-now]');
-console.log(digitBtns);
+const beforeScreenText = document.querySelector('[data-before]');
+const nowScreenText = document.querySelector('[data-now]');
+
+class Calculator {
+    constructor(beforeScreenText, nowScreenText) {
+        // assigning calculator display to class
+        this.beforeScreenText = beforeScreenText;
+        this.nowScreenText = nowScreenText;
+    }
+
+    showText(number) {
+        // update value
+        this.nowScreen = number;
+    }
+
+    refreshScreen() {
+        // show value on screen
+        this.nowScreenText.innerText = this.nowScreen;
+        console.log(this.nowScreen);
+    }
+}
+
+const calculator = new Calculator(beforeScreenText, nowScreenText);
+
+digitBtns.forEach(btn => {
+    // when digit btn is clicked get it's value and update screen
+    btn.addEventListener('click', () => {
+        calculator.showText(btn.innerText);
+        calculator.refreshScreen();
+    })
+})
